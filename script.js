6bd7d5f597612cfb735748211e7cde7d6d5a2211
neurici <neurici@gmail.com>
2025-12-26 17:02:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
-    LuxPDF: A free, open-source, and private PDF web application.
-    Copyright (C) 2025 LuxPDF
+    gPDF: A free, open-source, and private PDF web application.
+    Copyright (C) 2025 gPDF
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
